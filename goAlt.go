74eb2cd4a231ca12437779a421a135880207efbe
Philipp Neumann <PN@mtd-systems.de>
2020-11-17 20:14:17 +0100

    New simplified version that works with simple csv files

--- goAlt.go
+++ goAlt.go
@@ -2,63 +2,43 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
+	"encoding/csv"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 	"text/template"
 )
 
-type Alt struct {
+const (
+	startLine = 3
+	mainCol   = 0
+	altStart  = 2
+)
+
+type alt struct {
 	Name  string
 	Comma bool
 }
 
-type Character struct {
+type character struct {
 	Main string
 	Alts []string
 }
 
-type CharExport struct {
+type charExport struct {
 	Main   string
-	Alts   []Alt
+	Alts   []alt
 	IsMain bool
 	Comma  bool
 }
 
-func (c *Character) Save() error {
-	b, err := json.MarshalIndent(&c, "", "    ")
-	if err != nil {
-		return err
-	}
-	ioutil.WriteFile("chars/"+c.Main+".json", b, 0644)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *Character) Load(f string) error {
-	body, err := ioutil.ReadFile("chars/" + f)
-	if err != nil {
-		return err
-	}
-	err = json.Unmarshal(body, &c)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *Character) GetExport() []CharExport {
-	var res []CharExport
+func (c *character) getExport() []charExport {
+	var res []charExport
 	//Erstmal den Main Char
-	cm := CharExport{Main: c.Main, Comma: true}
+	cm := charExport{Main: c.Main, Comma: true}
 	for _, a := range c.Alts {
-		al := Alt{Name: a, Comma: true}
+		al := alt{Name: a, Comma: true}
 		cm.Alts = append(cm.Alts, al)
 	}
 	cm.Alts[len(cm.Alts)-1].Comma = false
@@ -67,14 +47,14 @@ func (c *Character) GetExport() []CharExport {
 
 	//Dann der Rest
 	for _, c1 := range c.Alts {
-		cres := CharExport{Main: c1}
+		cres := charExport{Main: c1}
 		for _, c2 := range c.Alts {
 			if c2 != c1 {
-				al := Alt{Name: c2, Comma: true}
+				al := alt{Name: c2, Comma: true}
 				cres.Alts = append(cres.Alts, al)
 			}
 		}
-		al := Alt{Name: c.Main, Comma: false}
+		al := alt{Name: c.Main, Comma: false}
 		cres.Alts = append(cres.Alts, al)
 		cres.IsMain = false
 		cres.Comma = true
@@ -84,61 +64,25 @@ func (c *Character) GetExport() []CharExport {
 }
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	text := "blub"
-	var c Character
-	err := c.Load("Gullofbog.json")
-	if err != nil {
-		log.Fatal(err)
+	var filename string
+	if len(os.Args) > 1 {
+		filename = os.Args[1]
+	} else {
+		filename = "characters.csv"
 	}
-
-	for text != "q\n" {
-		fmt.Println("===== Alttracker Generation ======")
-		fmt.Println("Please choose a Command:")
-		fmt.Println("1) Enter a New Character")
-		fmt.Println("2) Generate Plugindata")
-		fmt.Println("q) Exit")
-		text, _ = reader.ReadString('\n')
-		switch text {
-		case "1\n":
-			enterCharacter(reader)
-		case "2\n":
-			generatePlugindata()
-		}
-	}
+	generatePlugindata(filename)
 }
 
-func enterCharacter(reader *bufio.Reader) {
-	var c Character
-	fmt.Println("Please enter Main Character:")
-	text, _ := reader.ReadString('\n')
-	text = strings.Replace(text, "\n", "", -1)
-	c.Main = text
-	for text != "" {
-		fmt.Println("Please enter Alt Character:")
-		text, _ = reader.ReadString('\n')
-		text = strings.Replace(text, "\n", "", -1)
-		if text != "" {
-			c.Alts = append(c.Alts, text)
-		}
-	}
-	err := c.Save()
-	if err != nil {
-		log.Println("Error:", err)
-	}
-
-}
-
-func generatePlugindata() {
-	Chars := loadAllChars()
+func generatePlugindata(filename string) {
+	Chars := loadAllChars(filename)
 
 	t, err := template.ParseFiles("template.txt")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error parsing template:", err)
 	}
 	outfile, err := os.Create("AltTracker.plugindata")
 	if err != nil {
-		log.Println(err)
+		log.Println("Error parsing plugindata:", err)
 	}
 	fmt.Println(Chars)
 	w := bufio.NewWriter(outfile)
@@ -146,17 +90,36 @@ func generatePlugindata() {
 	w.Flush()
 }
 
-func loadAllChars() []CharExport {
-	var res []CharExport
-	files, _ := ioutil.ReadDir("chars")
-	for _, f := range files {
-		if filepath.Ext(f.Name()) == ".json" {
-			c := Character{}
-			c.Load(f.Name())
-			add := c.GetExport()
-			res = append(res, add...)
-		}
+func loadAllChars(filename string) []charExport {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal("Error opening File:", err)
 	}
-	res[len(res)-1].Comma = false
+	var res []charExport
+	line := 0
+	r := csv.NewReader(bufio.NewReader(f))
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		line++
+		if (line <= startLine) || (record[mainCol] == "") {
+			continue
+		}
+		var c character
+		c.Main = record[mainCol]
+		col := altStart
+		for {
+			if record[col] != "" {
+				c.Alts = append(c.Alts, record[col])
+				col++
+			} else {
+				break
+			}
+		}
+		fmt.Println(c)
+		res = append(res, c.getExport()...)
+	}
 	return res
 }
